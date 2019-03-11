c4e9e9e596ec9655c274747d6cf8d00fadc2fefa
vasesen <918811737@qq.com>
2019-03-12 00:19:08 +0800

    按钮添加icon

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,16 @@
-<template>
-    <button class="z-button">按钮</button>
+<template>    
+    <button class="z-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>       
+    </button>
 </template>
 <script>
 export default {
-
+    props:['icon','icon-position']
 }
 </script>
 <style lang="scss">
@@ -14,16 +21,24 @@ export default {
         border-radius: var(--boder-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
         &:hover{
             border-color: var(--border-color-hover)
         }
 
-        &:active{
-            background: var(--button-active-bg)
-        }
+        &:active{background: var(--button-active-bg)}
 
-        &:focus{
-            outline: none;
+        &:focus{outline: none;}
+
+        > .icon{order: 1; margin-right: .3em}
+        > .content{order: 2;}
+
+        &.icon-right{
+            > .icon{order: 2;}
+            > .content{order: 1;margin-right:0;margin-left: .3em}
         }
     }
     
