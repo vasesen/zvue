c93132821337ae3a8cc99ca93563d384482f0059
Vasesen <918811737@qq.com>
2019-09-26 00:19:30 +0800

    input组件

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Button from './Button'
 import Icon from './icon'
 import ButtonGroup from './button-group.vue'
+import Input from './input.vue'
 
 
 Vue.component('z-button',Button)
 Vue.component('z-icon',Icon)
 Vue.component('z-button-group',ButtonGroup)
+Vue.component('z-input',Input)
 new Vue({
     el:'#app',
     data:{
@@ -15,10 +17,10 @@ new Vue({
     }
 })
 
-import chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
-const expect = chai.expect
+// import chai from 'chai'
+// import spies from 'chai-spies'
+// chai.use(spies)
+// const expect = chai.expect
 //单元测试++
 // {
 //     const Constructor = Vue.extend(Button)
@@ -36,48 +38,48 @@ const expect = chai.expect
 //     button.$destroy()
 // }
 
-{
-    const Constructor = Vue.extend(Button)
-    const button = new Constructor({
-        propsData:{
-            icon:'settings',
-            loading:true
-        }
-    })
-    button.$mount(`#test`)
-    let useElement = button.$el.querySelector('use')
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-loading')
-    button.$el.remove()
-    button.$destroy()
-}
+// {
+//     const Constructor = Vue.extend(Button)
+//     const button = new Constructor({
+//         propsData:{
+//             icon:'settings',
+//             loading:true
+//         }
+//     })
+//     button.$mount(`#test`)
+//     let useElement = button.$el.querySelector('use')
+//     let href = useElement.getAttribute('xlink:href')
+//     expect(href).to.eq('#i-loading')
+//     button.$el.remove()
+//     button.$destroy()
+// }
 
-{
-    const div =  document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const button = new Constructor({
-        propsData:{
-            icon:'settings'
-        }
-    })
-    button.$mount(div)
-    let svg = button.$el.querySelector('svg')
-    let {order} = window.getComputedStyle(svg)
-    expect(order).to.eq('1')
-}
+// {
+//     const div =  document.createElement('div')
+//     document.body.appendChild(div)
+//     const Constructor = Vue.extend(Button)
+//     const button = new Constructor({
+//         propsData:{
+//             icon:'settings'
+//         }
+//     })
+//     button.$mount(div)
+//     let svg = button.$el.querySelector('svg')
+//     let {order} = window.getComputedStyle(svg)
+//     expect(order).to.eq('1')
+// }
 //click事件测试
-{
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData:{
-            icon:'settings'
-        }
-    })
-    vm.$mount()
-    let spy = chai.spy(function(){})
-    vm.$on('click',spy)
-    let button = vm.$el
-    button.click()
-    expect(spy).to.have.been.called()
-}
+// {
+//     const Constructor = Vue.extend(Button)
+//     const vm = new Constructor({
+//         propsData:{
+//             icon:'settings'
+//         }
+//     })
+//     vm.$mount()
+//     let spy = chai.spy(function(){})
+//     vm.$on('click',spy)
+//     let button = vm.$el
+//     button.click()
+//     expect(spy).to.have.been.called()
+// }
---
+++ src/input.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="wrapper">
+    <input :value="value" type="text" :disabled="disabled" :readonly="readonly" >
+    </div>
+</template>
+<script>
+export default {
+    name:'ZvInput',
+    props:{
+        value:{
+            type:String
+        },
+        disabled:{
+            type:Boolean,
+            default:false
+        },
+        readonly:{
+            type:Boolean,
+            default:false
+        },
+        error:{
+            type:String
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    $height:32px;
+    $border-color:#999;
+    $border-color-hover:#666;
+    $border-radius:4px;
+    $font-size:12PX;
+    $box-shadow-color:rgba(0,0,0,0.5);
+    .wrapper{
+        >input{
+            height:$height;
+            border: 1px solid $border-color;    
+            border-radius: $border-radius;
+            padding: 0 8px;
+            font-size: inherit;
+            &:hover{
+                border-color:$border-color-hover;
+            }
+            &:focus{
+                box-shadow: inset 0 1px 3px $box-shadow-color;
+                outline: none
+            }
+            &[disabled]{
+                border-color: #aaaaaa;
+                color: #aaa;
+                cursor: not-allowed;
+            }
+        }
+    }
+</style>
