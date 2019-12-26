9ebe39f798fff5476d5ac846195f89c13652f115
Vasesen <918811737@qq.com>
2019-12-27 02:42:44 +0800

    基本完成row 和col 组件

--- src/layout/col.vue
+++ src/layout/col.vue
@@ -1,5 +1,6 @@
-<template>
-    <div class="col" :class="[`col-${span}`]">
+ <template>
+    <div class="col" :class="colClasses" :style="colStyle">
+    <div style="background:yellow"></div>
         <slot></slot>
     </div>
 </template>
@@ -9,20 +10,42 @@ export default {
     props:{
         span:{
             type:[Number,String]
+        },
+        offset:{
+            type:[Number,String]
+        },
+    },
+    data(){
+        return {
+            gutter:0
+        }
+    },
+    computed:{
+        colStyle(){
+           return {
+               paddingLeft:this.gutter/2+'px' ,
+               paddingRight:this.gutter/2+'px'
+           }        
+        },
+        colClasses(){
+            let {span ,offset} = this
+            return  [span && `col-${span}`, offset && `offset-${offset}`]
         }
     }
 }
 </script>
  <style lang="scss" scoped>
  .col{
-     height: 100px;
-     width: 50%;
-     border:1px solid red;
-    
     $class-prefix: col-;
     @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n}{
             width: ($n/24)*100%
+        }
+    }
+    $class-prefix:offset-;
+    @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n}{
+            margin-left: ($n/24)*100%
         }
     }
  }
--- src/layout/row.vue
+++ src/layout/row.vue
@@ -1,13 +1,53 @@
 <template>
-    <div class="row">
+    <div class="row" :class="rowClass"
+    :style=" rowStyle">
         <slot></slot>
     </div>
 </template>
 <script>
-
+ export default{
+     name:'ZRow',
+     props:{
+         gutter:{
+             type :[Number,String]
+         },
+         align:{
+             type:String,
+             validator(){
+                return  ['left','right','center'].includes(value)
+             }
+         }
+     },
+     mounted(){
+         this.$children.forEach((vm)=>{
+             vm.gutter = this.gutter
+         })
+     },
+     computed:{
+         rowStyle(){
+             return{
+                marginLeft: -this.gutter/2+'px',
+                marginRight:-this.gutter/2+'px'
+             }
+         },
+         rowClass(){
+             let {align} = this
+             return [align && `align-${align} `]
+         }
+     }
+ }
 </script>
 <style lang="scss" scoped>
 .row{
     display:flex;
+    &.align-left{
+        justify-content:  flex-start;
+    }
+    &.align-right{
+        justify-content:flex-end;
+    }
+    &.align-center{
+        justify-content: center
+    }
 }
-</style>
+</style> 
