36e2f770c21fdeb648d67b077c0e3dab686978c0
Vasesen <918811737@qq.com>
2019-12-29 14:06:51 +0800

    完善测试用例

--- test/row.test.js
+++ test/row.test.js
@@ -10,7 +10,7 @@ describe('Row',()=>{
     it('存在', ()=>{
         expect(Row).to.exist
     })
-    it('接受 gutter 属性', (done)=>{
+    it('接收 gutter 属性', (done)=>{
         const div = document.createElement('div')
         Vue.component('g-row',Row)
         Vue.component('g-col',Col)
@@ -29,9 +29,25 @@ describe('Row',()=>{
             expect(getComputedStyle(row).marginLeft).to.eq('-10px')
             expect(getComputedStyle(row).marginRight).to.eq('-10px')
             const cols = vm.$el.querySelectorAll('.col')
-            expect(getComputedStyle[cols[0]].paddingLeftRight).to.eq('10px')
+            expect(getComputedStyle[cols[0]].paddingRight).to.eq('10px')
             expect(getComputedStyle[cols[0]].paddingLeft).to.eq('10px')
             done()
+            vm.$el.removw()
+            vm.$destroy()
         },1200)
+    })
+    it('接收 align 属性',()=>{
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Row)
+        const vm  = new Constructor({
+            propsData:{
+                align:'center'
+            }
+        }).$mount(div)
+        const element = vm.$el
+        expect(getComputedStyle(element).justifyContent).to.eq('center')
+        div.removw()
+        vm.$destroy()
     })
 })
