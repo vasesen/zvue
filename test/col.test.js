bb59d733318e89d7353abd8a29e4d2dd80c4d7e6
Vasesen <918811737@qq.com>
2019-12-29 15:41:16 +0800

    col 测试用例

---
+++ test/col.test.js
@@ -0,0 +1,41 @@
+const expect  = chai.expect
+import Vue from "vue"
+import Row from '../src/grid/row'
+import Col from '../src/grid/col'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Col',()=>{
+    it('存在',()=>{
+        expect(Col).to.exist
+    })
+    it('接收span属性',()=>{
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Col)
+        const vm = new Constructor({
+            propsData:{
+                span:1
+            }
+        }).$mount(div)
+        const element = vm.$el
+        expect(vm.$el.classList.contains('col-l')).to.eq(true)
+        div.remove()
+        vm.$destroy()
+    })
+    it('接收offset属性',()=>{
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Col)
+        const vm = new Constructor({
+            propsData:{
+                span:1
+            }
+        }).$mount(div)
+        const element = vm.$el
+        expect(vm.$el.classList.contains('offset-l')).to.eq(true)
+        div.remove()
+        vm.$destroy()
+    })
+})
